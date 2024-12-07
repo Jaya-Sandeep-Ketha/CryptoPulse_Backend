9789f60b8957c849bce2160ada0b0d7d2be48da6
Jaya-Sandeep-Ketha <140141084+Jaya-Sandeep-Ketha@users.noreply.github.com>
2024-12-06 21:43:13 -0500

    Backend Start

---
+++ src/main/java/com/sandy/cryptopulse/CryptoPulse/CryptoPulseApplication.java
@@ -0,0 +1,13 @@
+package com.sandy.cryptopulse.CryptoPulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoPulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandy/cryptopulse/CryptoPulse/CryptoPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandy.cryptopulse.CryptoPulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
