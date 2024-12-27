5348c610f20c41d90f5ae45d943ecd60f06a1556
”Jaya-Sandeep-Ketha” <kethajayasandeep1254@gmail.com>
2024-12-27 02:24:50 -0500

    Frontend link

--- src/main/java/com/sandy/config/AppConfig.java
+++ src/main/java/com/sandy/config/AppConfig.java
@@ -90,7 +90,7 @@ public class AppConfig {
 	                    "http://localhost:5173",
 						"http://localhost:5174",
 	                    "http://localhost:4200",
-							"https://zosh-treading.vercel.app"
+							"https://cryptopulse-trading.netlify.app"
 	                ));
 	                cfg.setAllowedMethods(Collections.singletonList("*"));
 	                cfg.setAllowCredentials(true);
